69cf1b99efbd6b1dec968f08c421e6de67057046
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2025-08-29 21:49:25 -0300

    sim

--- poo/src/main/java/com/superior/poo/PooApplication.java
+++ poo/src/main/java/com/superior/poo/PooApplication.java
@@ -2,12 +2,21 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-
+ @SpringBootApplication
+@RestController
 public class PooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PooApplication.class, args);
 	}
+
+
+
+@GetMapping("/hello")
+public String hello(){
+ return "Hello Word!!!!";
+}
 }
