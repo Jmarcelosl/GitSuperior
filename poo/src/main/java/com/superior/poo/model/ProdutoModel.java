1351b45efee1421b7a7be8193572047891ca2a98
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2025-09-02 20:05:12 -0300

    sim

--- poo/src/main/java/com/superior/poo/model/ProdutoModel.java
+++ poo/src/main/java/com/superior/poo/model/ProdutoModel.java
@@ -7,4 +7,12 @@ public class ProdutoModel {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CDPRODUTO")
     private int cdProduto;
+    @Column(name = "NMPRODUTO")
+    private String nmProduto;
+    @Column(name = "DSPRODUTO")
+    private String dsProduto;
+    @Column(name = "VLPRODUTO")
+    private double vlProduto;
+    @Column(name = "FLATIVO")
+    private String flativo;
 }
