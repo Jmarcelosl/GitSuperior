58a9c53f52edefdbc3936543566f8e5fc59ca936
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2025-09-02 21:38:05 -0300

    sim

--- poo/src/main/java/com/superior/poo/dto/ProdutoDto.java
+++ poo/src/main/java/com/superior/poo/dto/ProdutoDto.java
@@ -1,6 +1,19 @@
 package com.superior.poo.dto;
 
-public record ProdutoDto() {
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
 
-
-}
+public record ProdutoDto(
+    @NotBlank(message = "Não é possivel cadastrar o produto sem nome.") String nmProduto,
+    String dsProduto,
+    @NotNull(message = "Não é possível inserir o produto com o campo vazio.")
+    @DecimalMin(value = "0.01",
+            message = "Não é possivel cadastrar o produto com  valor abaixo de 0,01.")
+    double vlProduto,
+    @NotNull(message = "Não é possível inserir o produto com a flag vazia.")
+    @NotBlank(message = "Não é possivel cadastrar o produto com a flag em branco.")
+    String flativo
+) {}
